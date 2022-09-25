bf6fdbeb0294e7955cebeee6aceb85a759921bd8
memory <memory@debian11v1.home>
2022-09-25 09:40:27 -0400

    removed some gibrish

--- test.c
+++ test.c
@@ -1,1 +1,5 @@
-yes bebe
+#include <time.h>
+#include <stdlib.h>
+
+srand(time(NULL));   // Initialization, should only be called once.
+int r = rand();      // Returns a pseudo-random integer between 0 and RAND_MAX.
